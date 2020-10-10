7136c29591920da86ae520ba6590ecc3232d99c5
Stork W <njrogers311@yahoo.com>
2020-10-10 16:24:58 -0400

    re-arranged code to make it more fluid. Put password criteria and prompts into function. prompts are executed and information is stored when the generate button is pressed.

--- HW3script.js
+++ HW3script.js
@@ -1,29 +1,124 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-// Prompt Variables
-    
-    //let upperCharPrmpt = prompt("Include Upper Case Characters? (type yes / no)");
-   // let lowerCharPrmpt = prompt("include lower case characters? (type yes / no)");
-    //let spclCharPrmpt = prompt("Include special characters? (type yes / no)");
-    //let prmptArray = [upperCharPrmpt, lowerCharPrmpt, spclCharPrmpt, numPrmpt];
 
-function criteriaPrompt(){
-     
-     let upperCharPrmpt = prompt("Include Upper Case Characters? (type yes / no)");
-     let lowerCharPrmpt = prompt("include lower case characters? (type yes / no)");
-     let spclCharPrmpt = prompt("Include special characters? (type yes / no)");
-     let numPrmpt = prompt("Include numbers (type yes / no)")
-     let prmptArray = [upperCharPrmpt, lowerCharPrmpt, spclCharPrmpt, numPrmpt];
-     alert(prmptArray)
+
+
+
+// Array of special characters to be included in password
+var specialCharacters = [
+    '@',
+    '%',
+    '+',
+    '\\',
+    '/',
+    "'",
+    '!',
+    '#',
+    '$',
+    '^',
+    '?',
+    ':',
+    ',',
+    ')',
+    '(',
+    '}',
+    '{',
+    ']',
+    '[',
+    '~',
+    '-',
+    '_',
+    '.'
+  ];
   
-}
-let click = generateBtn.addEventListener('click', criteriaPrompt)
+  // Array of numeric characters to be included in password
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  
+  // Array of lowercase characters to be included in password
+  var lowerCasedCharacters = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ];
+  
+  // Array of uppercase characters to be included in password
+  var upperCasedCharacters = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z'
+  ];
 
-click
+  // Function prompting user for password criteria and variables to store user inputs.
+  function passwordCriteria(){
+    var pswrdLength = parseInt(
+        prompt('How many characters in password?')
+    );
+    var specialCharacters = confirm(
+        'Click ok to include special characters'
+    );
+    var numericCharacters = confirm(
+        'Click ok to include numeric characters'
+    );
+    var upperCharacters = confirm(
+        'Click ok to include upper cased characters'
+    );
+    var lowerCharacters = confirm(
+        'Click ok to include lower cased characters'
+    );
+    var criteriaOptions = {
+        pswrdLength : pswrdLength,
+        specialCharacters : specialCharacters,
+        numericCharacters : numericCharacters,
+        upperCharacters : upperCharacters,
+        lowerCharacters : lowerCasedCharacters
+    };
+  }
 
+  
+  var generateBtn = document.querySelector('#generate');
 
-
-    // prompt for loop
-
-
-
+  generateBtn.addEventListener('click', passwordCriteria)
