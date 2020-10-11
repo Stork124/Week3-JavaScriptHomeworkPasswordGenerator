6754f13e93fb8431c229704b9105fa9470fe4ded
Stork W <njrogers311@yahoo.com>
2020-10-10 21:36:53 -0400

    Added function for generator random password and displaying. add cal for the function that completes the entire task of generating the password.

--- HW3script.js
+++ HW3script.js
@@ -157,3 +157,12 @@ var specialCharacters = [
 
 
   var generateBtn = document.querySelector('#generate');
+
+  function writePassword(){
+      var password = createPassword();
+      var passwordText = document.querySelector('#password');
+
+      passwordText.value = password;
+  }
+
+  generateBtn.addEventListener('click', writePassword);
